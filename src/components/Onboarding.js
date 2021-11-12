16bf84fe2ff9785d5374245147143e5d7f0d27b9
Mehmood <m.hassan.rahi@gmail.com>
2021-11-12 13:59:25 +0500

    * Added OnBoardingComponent

---
+++ src/components/Onboarding.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import LogoSvg from '../assets/images/gaming.svg';
+
+export const Onboarding = props => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>GAMEON</Text>
+      <LogoSvg {...styles.logo} />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => props.navigation.navigate('Home')}>
+        <Text style={styles.buttonText}>Let's begin</Text>
+        <MaterialIcons name="arrow-forward-ios" size={22} color="#fff" />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 50,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#20315f',
+    fontFamily: 'Inter-Bold',
+  },
+  button: {
+    flexDirection: 'row',
+    backgroundColor: '#AD40AF',
+    width: '90%',
+    padding: 20,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: '#fff',
+    fontSize: 18,
+    fontFamily: 'Roboto-MediumItalic',
+  },
+  logo: {
+    height: 300,
+    width: 300,
+    transform: [{rotate: '-15deg'}],
+  },
+});
