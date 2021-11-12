1200efce7399e5263bff58a11ed3a9fd474158cd
Mehmood <m.hassan.rahi@gmail.com>
2021-11-12 12:41:06 +0500

    * Configured react navigation for android

--- android/app/src/main/java/com/gameon/MainActivity.java
+++ android/app/src/main/java/com/gameon/MainActivity.java
@@ -1,4 +1,5 @@
 package com.gameon;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 
@@ -12,4 +13,9 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "gameOn";
   }
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
 }
