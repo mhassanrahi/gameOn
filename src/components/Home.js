679e34af6addf94faa8bc4e4d4a482cff44df19d
Mehmood <m.hassan.rahi@gmail.com>
2021-11-12 13:59:04 +0500

    * Added home component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, SafeAreaView} from 'react-native';
+
+export const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
