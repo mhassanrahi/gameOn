6378925b2da802ef9237644be022a645da1f7b2e
Mehmood <m.hassan.rahi@gmail.com>
2021-11-12 12:16:22 +0500

    * Added button and text to the Home screen

--- App.js
+++ App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import {SafeAreaView, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
+import LogoSvg from './src/assets/images/gaming.svg';
+
 const App = () => {
   return (
-    <SafeAreaView
-      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 30, fontWeight: 'bold', color: '#20315f'}}>
+    <SafeAreaView style={styles.conatiner}>
+      <Text
+        style={{
+          fontSize: 30,
+          fontWeight: 'bold',
+          color: '#20315f',
+          fontFamily: 'Inter-Bold',
+        }}>
         GAMEON
       </Text>
+      <LogoSvg size={33} />
       <TouchableOpacity
         style={{
           flexDirection: 'row',
@@ -19,15 +27,23 @@ const App = () => {
           alignItems: 'center',
           borderRadius: 5,
         }}>
-        <Text style={{fontWeight: 'bold', color: '#fff', fontSize: 18}}>
+        <Text
+          style={{
+            fontWeight: 'bold',
+            color: '#fff',
+            fontSize: 18,
+            fontFamily: 'Roboto-MediumItalic',
+          }}>
           Let's begin
         </Text>
-        <MaterialIcons name="arrow-forward-ios" size={22} />
+        <MaterialIcons name="arrow-forward-ios" size={22} color="#fff" />
       </TouchableOpacity>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  conatiner: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+});
 
 export default App;
